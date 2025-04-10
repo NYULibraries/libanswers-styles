ea8251d053206c380e5246f9239b4af6b0f9380f
Vlad Burlutskiy <vlad.burlutskiy@icloud.com>
2025-04-10 10:57:57 -0400

    feat: initialize project with Vite, add base styles and main entry point

---
+++ src/main.js
@@ -0,0 +1,12 @@
+import './css/style.css'
+
+document.querySelector('#app').innerHTML = `
+  <div>
+    <h1>LibAnswers styles</h1>
+        <a href="https://github.com/NYULibraries/libanswers-styles/blob/main/README.md" target="_blank">
+      Click on the link to learn more
+    </a>
+  </div>
+`
+
+
